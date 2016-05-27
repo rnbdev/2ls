76551992f1d82403ad40a7cb800f4324a942037d
Kumar Madhukar <kumar.madhukar@tcs.com>
2016-05-27 19:06:20 +0530

    code for unwinding and inlining

--- src/summarizer/summary_checker_kind.cpp
+++ src/summarizer/summary_checker_kind.cpp
@@ -59,46 +59,137 @@ property_checkert::resultt summary_checker_kindt::operator()(
 	  f_it != it->second.functions.end(); f_it++){
 	local_SSAt &SSA = ssa_db.get(it->first);
 
-	for(local_SSAt::nodest::const_iterator n_it = SSA.nodes.begin();
+	std::list<local_SSAt::nodet> inline_nodes;
+	std::vector<equal_exprt> first_node_equalities;
+	int counter = ssa_inliner.get_rename_counter();
+	
+	for(local_SSAt::nodest::iterator n_it = SSA.nodes.begin();
 	    n_it != SSA.nodes.end(); n_it++){
-
+	  
 	  local_SSAt::nodet &node=*n_it;
+
 	  if(node.location == *(f_it)){
 
+	    bool clear_function_call = false;
+	    
 	    for(local_SSAt::nodet::function_callst::const_iterator fc_it=node.function_calls.begin();
 		fc_it!=node.function_calls.end(); fc_it++){
 	      
 	      irep_idt fname = to_symbol_expr(fc_it->function()).get_identifier();
 	      if(ssa_db.exists(fname))
 		{
-		  const local_SSAt &fSSA = ssa_db.get(fname);
+		  clear_function_call = true;
+		  
+		  local_SSAt &fSSA = ssa_db.get(fname);
 
 		  exprt guard_binding;
 		  exprt::operandst bindings_in, bindings_out;
-		  int counter = ssa_inliner.get_rename_counter();
-
+			  
+		  // put guard_binding, bindings_in, bindings_out in the caller's SSA (equalities)
 		  ssa_inliner.get_guard_binding(SSA,fSSA,n_it,guard_binding,counter);
 		  equal_exprt e = to_equal_expr(guard_binding);
 		  node.equalities.push_back(e);
 		  
-		  
 		  ssa_inliner.get_bindings(SSA,fSSA,n_it,fc_it,bindings_in,bindings_out,counter);
-		  // put guard_binding, bindings_in, bindings_out in the caller's SSA (equalities)
 
-		  // copy fSSA's each node's items (equalities, assertions, etc.)
-		  // into node's corresponding item (after renaming)
+		  for(exprt::operandst::const_iterator b_it=bindings_in.begin();
+		      b_it!=bindings_in.end(); b_it++){
+		    equal_exprt e = to_equal_expr(*b_it);
+		    node.equalities.push_back(e);
+		  }
+		  for(exprt::operandst::const_iterator b_it=bindings_out.begin();
+		      b_it!=bindings_out.end(); b_it++){
+		    equal_exprt e = to_equal_expr(*b_it);
+		    node.equalities.push_back(e);
+		  }
 
-		  
-		  
+		  for(local_SSAt::nodest::const_iterator fn_it = fSSA.nodes.begin();
+		      fn_it != fSSA.nodes.end(); fn_it++){
+		    local_SSAt::nodet fnode=*fn_it;
+		    inline_nodes.push_back(fnode);
+		  }
+
+		  if(fname == entry_function){
+		    //  first_node_equalities should contain all the equalities from the first node of fSSA
+		    for(local_SSAt::nodest::iterator fn_it = fSSA.nodes.begin();
+			fn_it != fSSA.nodes.end(); fn_it++){
+		      local_SSAt::nodet &fnode=*fn_it;
+		      for(local_SSAt::nodet::equalitiest::iterator e_it=fnode.equalities.begin();
+			  e_it!=fnode.equalities.end(); e_it++){
+			first_node_equalities.push_back(*e_it);
+		      }
+		      break;
+		    }
+		  }
+		  else{
+		    //  except those (the one) that start with "ssa::guard" and have true in the rhs
+		    for(local_SSAt::nodest::iterator fn_it = fSSA.nodes.begin();
+			fn_it != fSSA.nodes.end(); fn_it++){
+		      local_SSAt::nodet &fnode=*fn_it;
+		      for(local_SSAt::nodet::equalitiest::iterator e_it=fnode.equalities.begin();
+			  e_it!=fnode.equalities.end(); e_it++){
+			// unless lhs starts with "ssa::guard" and rhs is true
+
+			equal_exprt e = to_equal_expr(*e_it);
+			exprt &lhs = e.lhs(); exprt &rhs = e.rhs();
+			std::string var_string = id2string(to_symbol_expr(lhs).get_identifier());
+			if(((var_string.substr(0,11)) == "ssa::$guard") && (rhs.is_true())){
+			  // ignore the equality in this case
+			}
+			else{
+			  first_node_equalities.push_back(*e_it);
+			}
+			
+		      }
+		      break;
+		    }
+		  }
+
 		}
-		  
-		  
 	    }
+	    
+	    if(clear_function_call == true)
+	      node.function_calls.clear();
+		
 	  }
 	}
 
-
+	bool replace_first_node_equalities = true;
 	
+	if(inline_nodes.size() > 0){
+	  for(std::list<local_SSAt::nodet>::iterator in_it = inline_nodes.begin();
+	      in_it != inline_nodes.end(); in_it++){
+	    local_SSAt::nodet &inline_node = *in_it;
+
+	    if(replace_first_node_equalities == true){
+	      inline_node.equalities.clear();
+	      for(std::vector<equal_exprt>::iterator e_it=first_node_equalities.begin();
+		  e_it!=first_node_equalities.end(); e_it++){
+		inline_node.equalities.push_back(*e_it);
+	      }
+	      replace_first_node_equalities = false;
+	    }
+	    
+	    for(local_SSAt::nodet::equalitiest::iterator e_it=inline_node.equalities.begin();
+		e_it!=inline_node.equalities.end(); e_it++){
+	      ssa_inliner.rename(*e_it, counter);
+	    }
+	    
+	    for(local_SSAt::nodet::constraintst::iterator c_it=inline_node.constraints.begin();
+		c_it!=inline_node.constraints.end(); c_it++){
+	      ssa_inliner.rename(*c_it, counter);
+	    }
+
+	    for(local_SSAt::nodet::assertionst::iterator a_it=inline_node.assertions.begin();
+		a_it!=inline_node.assertions.end(); a_it++){
+	      ssa_inliner.rename(*a_it, counter);
+	    }
+
+	    // push inline_node into SSA
+	    SSA.nodes.push_back(inline_node);
+	    
+	  }
+	}
 	
       }
     }
